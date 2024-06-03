ac2fc197919b244844fac66519a9eac98c3c51dd
Katherine Cho <rheeys117@gmail.com>
2024-06-03 16:25:08 -0700

    components and css

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -1,11 +1,27 @@
-
+import React from 'react';
 import './App.css';
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Menu from './components/Menu';
+import Testimonial from './components/Testimonial';
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+
+
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Nav/>
+      <Header />
+      <Main />
+      <Menu/>
+      <Testimonial/>
+      <AboutUs/>
+      <Footer/>
+    </>
   );
 }
-
 export default App;
